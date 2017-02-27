07817339c1985c060e5aa2f57d389e748c063c1b
Jared Baribeau <jared.baribeau@gmail.com>
2017-02-27 13:50:10 -0500

    Initial commit of LaserManager

---
+++ LaserManager.py
@@ -0,0 +1,15 @@
+# LaserManager
+# UML: https://www.lucidchart.com/documents/edit/e6432d38-782d-40ea-b279-736218c36351/0
+
+xPosition = 0
+yPosition = 0
+
+global def setPosition(x, y):
+	# Do stuff
+
+global def getXPosition():
+	return xPosition
+
+global def getYPosition():
+	return yPosition
+
