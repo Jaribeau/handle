a5398dab358d0468eb58869046a0fde4688c8f0a
River Wong <rio11oir@gmail.com>
2017-03-06 14:49:52 -0500

    Update LaserManager.py
    Not ready yet, but getting there.

--- LaserManager.py
+++ LaserManager.py
@@ -1,6 +1,12 @@
 # LaserManager
 # UML: https://www.lucidchart.com/documents/edit/e6432d38-782d-40ea-b279-736218c36351/0
 
+
+import RPi.GPIO as GPIO
+import time
+import sys
+import math
+
 xPosition = 0
 yPosition = 0
 
@@ -13,3 +19,34 @@ global def getXPosition():
 global def getYPosition():
 	return yPosition
 
+PLAY_FIELD_WIDTH = 2
+PLAY_FIELD_LENGTH = 2
+CAM_DIST_HORI = 0.5
+CAM_DIST_VERT = 1.5
+
+if (len(sys.argv) != 2):
+    print 'Please input 2 arguments, the x position and y position as arguments.'
+    quit()
+else:
+    x = sys.argv[1]
+    y = sys.argv[2]
+
+
+myX = X - (PLAY_FIELD_WIDTH/2)
+myY = y + CAM_DIST_HORI
+myZ = CAM_DIST_VERT
+
+horiAngle = math.atan(myY/myX)
+vertAngle = math.acos(myY/math.sqrt(pow(myX,2),pow(myY,2),pow(myZ,2)))
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+pwm = GPIO.PWM(2, 100)
+pwm.start(5)
+
+
+    duty = float(angle) / 10.0 + 2.5
+    pwm.ChangeDutyCycle(duty)
+
+
+pwm.stop()
