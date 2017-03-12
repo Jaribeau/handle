4ebe4168d49fd3e9209212d3b1c0341f121cb909
River Wong <rio11oir@gmail.com>
2017-03-12 00:31:58 -0500

    Should control lasers properly
    Need to test with Pi (once I get the monitor back to loo)

--- LaserManager.py
+++ LaserManager.py
@@ -7,11 +7,33 @@ import time
 import sys
 import math
 
+PLAY_FIELD_WIDTH = 2
+PLAY_FIELD_LENGTH = 2
+CAM_DIST_HORI = 0.5
+CAM_DIST_VERT = 1.5
+
 xPosition = 0
 yPosition = 0
 
 global def setPosition(x, y):
-	# Do stuff
+	xPosition = x
+	yPosition = y
+
+	myX = xPosition - (PLAY_FIELD_WIDTH/2)
+	myY = yPosition + CAM_DIST_HORI
+	myZ = CAM_DIST_VERT
+
+	horiAngle = math.atan(myY/myX)
+	vertAngle = math.acos(myY/math.sqrt(pow(myX,2),pow(myY,2),pow(myZ,2)))
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(2, GPIO.OUT)
+	pwm = GPIO.PWM(2, 100)
+	pwm.start(5)
+
+
+	duty = float(angle) / 10.0 + 2.5
+	pwm.ChangeDutyCycle(duty)
 
 global def getXPosition():
 	return xPosition
@@ -19,34 +41,12 @@ global def getXPosition():
 global def getYPosition():
 	return yPosition
 
-PLAY_FIELD_WIDTH = 2
-PLAY_FIELD_LENGTH = 2
-CAM_DIST_HORI = 0.5
-CAM_DIST_VERT = 1.5
-
-if (len(sys.argv) != 2):
-    print 'Please input 2 arguments, the x position and y position as arguments.'
-    quit()
-else:
-    x = sys.argv[1]
-    y = sys.argv[2]
+global def stop():
+	pwm.stop()
 
 
-myX = X - (PLAY_FIELD_WIDTH/2)
-myY = y + CAM_DIST_HORI
-myZ = CAM_DIST_VERT
-
-horiAngle = math.atan(myY/myX)
-vertAngle = math.acos(myY/math.sqrt(pow(myX,2),pow(myY,2),pow(myZ,2)))
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-pwm = GPIO.PWM(2, 100)
-pwm.start(5)
 
 
-    duty = float(angle) / 10.0 + 2.5
-    pwm.ChangeDutyCycle(duty)
 
 
-pwm.stop()
+
