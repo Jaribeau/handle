0a1bfecf07e48f7c232ac9618807682900ec9b68
Jared Baribeau <jared.baribeaugmail.com>
2017-03-20 08:41:44 +0000

    fixes to the bouncing

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # main
 
-from GameManager import GameManager
+#from GameManager import GameManager
+from ObstacleManager import ObstacleManager
 
-game = GameManager()
-game.start_game()
+#game = GameManager()
+#game.start_game()
+
+obstacle = ObstacleManager()
+obstacle.start_movement()
+
+while True:
+    True
