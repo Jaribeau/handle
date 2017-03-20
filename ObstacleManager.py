28e66bcb926d1b54e6a13ccea8ebc639d1a1b2ee
Jared Baribeau <jared.baribeau@gmail.com>
2017-03-19 23:29:10 -0400

    Updated obstace to adhere to BallTrackers singleton pattern

--- ObstacleManager.py
+++ ObstacleManager.py
@@ -29,7 +29,7 @@ class ObstacleManager:
 
         self.laser = LaserManager()
         self.properties = Properties()
-        self.ballTracker = BallTracker()
+        self.ballTracker = BallTracker.get_instance()
 
 
     # called by GameManager
