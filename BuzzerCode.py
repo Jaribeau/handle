4395ee1318a233d5bd87640a1d1fbf59dc6f5a90
Jared Baribeau <jared.baribeaugmail.com>
2017-03-26 19:38:16 +0000

    Edited BuzzerCode to work with real buzzer

--- BuzzerCode.py
+++ BuzzerCode.py
@@ -1,7 +1,29 @@
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ENTER NUMBER, GPIO.OUT)
 
-buzzerPwm = GPIO.PWM(ENTER NUMBER, 100)
-p.start(0)
+dutyCycle = 15 # affects sound frequency
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+buzzerPwm = GPIO.PWM(21, 2000)
+
+buzzerSwitch = True
+
+if buzzerSwitch:
+    buzzerPwm.start(60)
+    time.sleep(0.5)
+    buzzerPwm.stop()
+    time.sleep(0.5)
+    buzzerPwm.start(60)
+    time.sleep(0.5)
+    buzzerPwm.stop()
+    time.sleep(0.5)
+    buzzerPwm.start(60)
+    time.sleep(0.5)
+    buzzerPwm.stop()
+else:
+    buzzerPwm.stop()
+        
+time.sleep(1)
+buzzerSwitch = False
